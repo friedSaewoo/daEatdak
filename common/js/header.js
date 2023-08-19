1d8ee3d5db4c6ca947f7e445f57eb6001cdf3a02
bokkaa <bokyh0327@gmail.com>
2023-08-19 13:08:24 +0900

    사이드배너 추가 및 상품 탭 글자 수정 중

--- common/js/header.js
+++ common/js/header.js
@@ -4,19 +4,25 @@ $(window).scroll(function () {
   });
   
 
-  // header부분 높이 초과시 fixed 메뉴바 등장
+  // header부분 높이 초과시 fixed 메뉴바 및 사이드 배너 영역 등장
   $(document).ready(function(){
     var headerHeight = $(".header").height(); 
   console.log(headerHeight);
     $(".nav-fix-menu-container").hide();
+
     $(window).scroll(function(){  
         var fixedMenuShow = $(this).scrollTop() >= headerHeight; 
   
-    if(fixedMenuShow){ 
+
+    if(fixedMenuShow==0){
+      $("#floatMenu-container").hide();
+      $(".nav-fix-menu-container").hide();
+    }else if(fixedMenuShow){ 
             $(".nav-fix-menu-container").show();
-        }
-        else{
+            $("#floatMenu-container").fadeIn();
+    }else{
             $(".nav-fix-menu-container").hide();
+            $("#floatMenu-container").fadeOut();
         }
     });
     
