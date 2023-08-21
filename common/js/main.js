e5f2bbbd390dac3bca77361f7d7c53f08c2acf5e
bokkaa <bokyh0327@gmail.com>
2023-08-21 13:23:26 +0900

    헤더 푸터 메인페이지 v2.2

--- common/js/main.js
+++ common/js/main.js
@@ -102,46 +102,46 @@ startTimer();
 
 
 
-//상품들 영역 화살표 좌우슬라이드
-  let $slideBox = $('.goods-contents ul');
-  let $slideImgs = $('.goods-contents ul img');
-  let slideWidth = 1051;
-  let currentIdx = 0;
-  let slideCnt = $slideImgs.length;
-  console.log('slideCnt : ' + slideCnt);
-  checkEnd()
+// //상품들 영역 화살표 좌우슬라이드
+//   let $slideBox = $('.goods-contents ul');
+//   let $slideImgs = $('.goods-contents ul img');
+//   let slideWidth = 1051;
+//   let currentIdx = 0;
+//   let slideCnt = $slideImgs.length;
+//   console.log('slideCnt : ' + slideCnt);
+//   checkEnd()
   
-  $('.goods-right-button').stop().on('click', function () {
-      console.log('aa');
-      currentIdx++;
-      console.log('currentIdx : ' + currentIdx);
-      $slideBox.css('left', -(currentIdx * slideWidth));
-      $slideBox.css('transition', '0.5s ease');
-      checkEnd()
-      });
+//   $('.goods-right-button').stop().on('click', function () {
+//       console.log('aa');
+//       currentIdx++;
+//       console.log('currentIdx : ' + currentIdx);
+//       $slideBox.css('left', -(currentIdx * slideWidth));
+//       $slideBox.css('transition', '0.5s ease');
+//       checkEnd()
+//       });
   
-  $('.goods-left-button').on('click', function () {
-    console.log('bbb');
-    currentIdx--;
-    console.log('currentIdx : ' + currentIdx);
-    $slideBox.css('left', -(currentIdx * slideWidth));
-    $slideBox.css('transition', '0.5s ease');
-    checkEnd()
-  });
+//   $('.goods-left-button').on('click', function () {
+//     console.log('bbb');
+//     currentIdx--;
+//     console.log('currentIdx : ' + currentIdx);
+//     $slideBox.css('left', -(currentIdx * slideWidth));
+//     $slideBox.css('transition', '0.5s ease');
+//     checkEnd()
+//   });
   
-  function checkEnd() {
-    if (currentIdx <= 0) {
-      $('.goods-left-button').css('display', 'none');
-    } else {
-      $('.goods-left-button').css('display', 'block');
-    }
+//   function checkEnd() {
+//     if (currentIdx <= 0) {
+//       $('.goods-left-button').css('display', 'none');
+//     } else {
+//       $('.goods-left-button').css('display', 'block');
+//     }
   
-    if (currentIdx==1) {
-      $('.goods-right-button').css('display', 'none');
-    } else {
-      $('.goods-right-button').css('display', 'block');
-    }
-  }
+//     if (currentIdx==1) {
+//       $('.goods-right-button').css('display', 'none');
+//     } else {
+//       $('.goods-right-button').css('display', 'block');
+//     }
+//   }
 
 
 
